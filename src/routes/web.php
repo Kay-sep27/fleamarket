da551edd04bd027355e6951e0ad95cf8051b98fa
Kay <kayo.pi@icloud.com>
2025-07-01 22:54:23 +0900

    add: contactページのphpとcssファイルを作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/confirm', function () {
+    return view('confirm');
+});
+Route::get('/thanks', function () {
+    return view('thanks');
+});
+Route::get('/admin', function () {
+    return view('admin');
 });
